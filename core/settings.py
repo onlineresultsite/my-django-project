2d3c8be9d41acfb6833d26d217e62cd4ce3b9087
onlineresultsite <footflux@gmail.com>
2024-07-29 12:25:13 +0530

    deploy to ec2

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 #  --------------------------==========-------------------------------
 
 ALLOWED_HOSTS = ['*']
@@ -95,7 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'  # Note the leading slash
